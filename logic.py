45ddbb8c21bfa35b98ff15acf33190d0b7baa0c6
LIONELORFINO <lionelyustanlie69@gmail.com>
2025-10-18 17:43:06 +0800

    Create logic.py

---
+++ logic.py
@@ -0,0 +1,87 @@
+import aiohttp  # A library for asynchronous HTTP requests
+import random
+from datetime import datetime, timedelta
+class Pokemon:
+    pokemons = {}
+    # Object initialisation (constructor)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        self.ability = None
+        self.last_feed_time = datetime.now()
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+        # An asynchronous method to get the name of a pokémon via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['forms'][0]['name']  # Returning a Pokémon's name
+                else:
+                    return "Pikachu"  # Return the default name if the request fails
+    
+    async def info(self):
+        # A method that returns information about the pokémon
+        if not self.name:
+            self.name = await self.get_name() 
+        self.ability = await self.get_ability() # Retrieving a name if it has not yet been uploaded
+        return f"The name of your Pokémon: {self.name} ability {self.ability}"  # Returning the string with the Pokémon's name
+
+    async def show_img(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['sprites']['front_default']  # Returning a Pokémon's name
+                else:
+                    return "No image"  # Return the default name if the request fails
+                
+    async def get_ability(self):
+        # An asynchronous method to retrieve the URL of a pokémon image via PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API for the request
+        async with aiohttp.ClientSession() as session:  # Opening an HTTP session
+            async with session.get(url) as response:  # Sending a GET request
+                if response.status == 200:
+                    data = await response.json()  # Receiving and decoding JSON response
+                    return data['abilities'][0]['ability']['name']  # Returning a Pokémon's name
+                else:
+                    return "No ability"  # Return the default name if the request fails
+                
+    async def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
+        else:
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
+        
+class Wizard(Pokemon):
+    async def feed(self, feed_interval = 10, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
+        else:
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
+class Fighter(Pokemon):
+    async def feed(self, feed_interval = 20, hp_increase = 20 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(hours=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Kesehatan Pokemon dipulihkan. HP saat ini: {self.hp}"
+        else:
+            return f"Kalian dapat memberi makan Pokémon kalian di: {current_time-delta_time}"
